14e66a222e90cf5497032e7d6768f1e7ca49bb73
missymogara <monicagara97@gmail.com>
2024-10-17 16:32:17 +0200

    small hotfix

--- php/Siete-y-media/controlador.php
+++ php/Siete-y-media/controlador.php
@@ -1,75 +1,76 @@
-<?php 
+<?php
 session_start();
 include "cartas.php";
 include "lib.php";
 
 // Initialize total matches
-if (!isset($_SESSION['totalMatches'])){
+if (!isset($_SESSION['totalMatches'])) {
     $_SESSION['totalMatches'] = 0;
 }
 
 
 // Initialize won matches
-if (!isset($_SESSION['wonMatches'])){
+if (!isset($_SESSION['wonMatches'])) {
     $_SESSION['wonMatches'] = 0;
 }
 
 
 // Initialize lost matches
-if (!isset($_SESSION['lostMatches'])){
+if (!isset($_SESSION['lostMatches'])) {
     $_SESSION['lostMatches'] = 0;
 }
 
 // Initialize game status
-if (!isset($_SESSION["gameStatus"])){
+if (!isset($_SESSION["gameStatus"])) {
     $_SESSION["gameStatus"] = "none";
 }
 
 // Initialize the score
-if(!isset($_SESSION["actualScore"])){
+if (!isset($_SESSION["actualScore"])) {
     $_SESSION["actualScore"] = 0;
 }
 
 // Create the hand empty
 if (!isset($_SESSION["cardsDisplayed"])) {
     $_SESSION["cardsDisplayed"] = array();
-    
 };
 
-// Action control
-if (isset($_GET['action'])){
-    // Draw card from deck
-    if (strcmp($_GET['action'], "draw") == 0){
-        
-        
-        if (!strcmp($_SESSION["gameStatus"], "lost") == 0 
-        && !strcmp($_SESSION["gameStatus"], "won") == 0){ // If game is lost you can't draw cards
-            drawCard($_SESSION["deck"]);
-            checkScoreHand();
-            checkGameStatus($_SESSION["actualScore"]); // Check game status
-        }
-        header("Location: juego.php");
-    };
-    // Reset the game
-    if (strcmp($_GET['action'], "reset") == 0){
-        //session_destroy();
-        $_SESSION["cardsDisplayed"] = array();
-        $_SESSION["actualScore"] = 0;
-        $_SESSION["gameStatus"] = "none";
-        header("Location: juego.php");
-    };
-    // Reset session if you want to check how it works in a new session
-    if (strcmp($_GET['action'], "resetsessison") == 0){
-        session_destroy();
-        session_start();
-        $_SESSION["cardsDisplayed"] = array();
-        $_SESSION["actualScore"] = 0;
-        $_SESSION["gameStatus"] = "none";
-        $_SESSION['totalMatches'] = 0;
-        $_SESSION['wonMatches'] = 0;
-        $_SESSION['lostMatches'] = 0;
-        header("Location: juego.php");
-    };
-};
+if ($_GET) {
+    // Action control
+    if (isset($_GET['action'])) {
+        // Draw card from deck
+        if (strcmp($_GET['action'], "draw") == 0) {
 
-?>
+
+            if (
+                !strcmp($_SESSION["gameStatus"], "lost") == 0
+                && !strcmp($_SESSION["gameStatus"], "won") == 0
+            ) { // If game is lost you can't draw cards
+                drawCard($_SESSION["deck"]);
+                checkScoreHand();
+                checkGameStatus($_SESSION["actualScore"]); // Check game status
+            }
+            header("Location: juego.php");
+        };
+        // Reset the game
+        if (strcmp($_GET['action'], "reset") == 0) {
+            //session_destroy();
+            $_SESSION["cardsDisplayed"] = array();
+            $_SESSION["actualScore"] = 0;
+            $_SESSION["gameStatus"] = "none";
+            header("Location: juego.php");
+        };
+        // Reset session if you want to check how it works in a new session
+        if (strcmp($_GET['action'], "resetsessison") == 0) {
+            session_destroy();
+            session_start();
+            $_SESSION["cardsDisplayed"] = array();
+            $_SESSION["actualScore"] = 0;
+            $_SESSION["gameStatus"] = "none";
+            $_SESSION['totalMatches'] = 0;
+            $_SESSION['wonMatches'] = 0;
+            $_SESSION['lostMatches'] = 0;
+            header("Location: juego.php");
+        };
+    };
+}
