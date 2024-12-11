977976ba2ddbb7069cb0bac65ab15618509c3c2e
missymogara <monicagara97@gmail.com>
2024-12-11 17:13:53 +0100

    small update in js version

--- php/chatgpt-mvc/vistas/head.php
+++ php/chatgpt-mvc/vistas/head.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ChatGPT-API</title>
     <link rel="stylesheet" href="./vistas/assets/css/styles.css?version=1.1">
-    <script src="./public/js/app.js?v=1.1" defer type="module"></script>
+    <script src="./public/js/app.js?v=1.2" defer type="module"></script>
 </head>
